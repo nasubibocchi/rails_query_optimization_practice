064f4f7820e4b9ef212fae07788ac1e154060576
nasubibocchi <matsuhito125@gmail.com>
2025-08-02 17:16:11 +0900

    問題3

--- app/services/export_service.rb
+++ app/services/export_service.rb
@@ -3,23 +3,36 @@
 
 class ExportService
   def self.export_user_activity
-    users = User.includes(:posts, :comments)
+    # users = User.includes(:posts, :comments)
+    posts = Post.eager_load(:user).preload(:comments)
     
     csv_data = []
-    users.each do |user|
-      user.posts.each do |post|
-        post.comments.each do |comment|
-          csv_data << [
-            user.name,
-            user.email,
-            post.title,
-            comment.content,
-            comment.created_at
-          ]
-        end
+    # users.each do |user|
+    #   user.posts.each do |post|
+    #     post.comments.each do |comment|
+    #       csv_data << [
+    #         user.name,
+    #         user.email,
+    #         post.title,
+    #         comment.content,
+    #         comment.created_at
+    #       ]
+    #     end
+    #   end
+    # end
+    
+    posts.each do |post|
+      post.comments.each do |comment|
+        csv_data << [
+          post.user.name,
+          post.user.email,
+          post.title,
+          comment.content,
+          comment.created_at
+        ]
       end
     end
-    
+
     csv_data
   end
 end
