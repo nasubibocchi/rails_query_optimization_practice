b88c5960650526550471f58ea472e458cad3e015
nasubibocchi <matsuhito125@gmail.com>
2025-08-02 11:27:32 +0900

    syntax error修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_many :posts, dependant: :destroy
   has_many :comments, dependant: :destroy
 
-  scope: :active, -> { where(status: 'active') }
-  scope: :inactive, -> { where(status: 'inactive') }
+  scope :active, -> { where(status: 'active') }
+  scope :inactive, -> { where(status: 'inactive') }
 end
