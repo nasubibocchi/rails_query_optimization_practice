659eb64b7b08aa4afdc8847226006aa0c2702a71
nasubibocchi <matsuhito125@gmail.com>
2025-08-02 18:45:47 +0900

    問題5

--- app/services/posts_service.rb
+++ app/services/posts_service.rb
@@ -1,16 +1,22 @@
 class PostsService
   # 問題5: 関連データの条件付き読み込み
   def self.posts_with_recent_comments
-    posts = Post.published.includes(:comments)
-    
+    # posts = Post.published.includes(:comments)
+    posts = Post.published.eager_load(:user, :comments)
+      .merge(User.active)
+      .merge(Comment.approved)
+      .distinct
+
     result = posts.map do |post|
-      recent_comments = post.comments
-                           .joins(:user)
-                           .where(status: 'approved', users: { status: 'active' })
-                           .order(created_at: :desc)
-                           .limit(3)
-                           .includes(:user)
-      
+      # recent_comments = post.comments
+      #                      .joins(:user)
+      #                      .where(status: 'approved', users: { status: 'active' })
+      #                      .order(created_at: :desc)
+      #                      .limit(3)
+      #                      .includes(:user)
+
+      recent_comments = post.comments.order(created_at: :desc).limit(3)
+
       {
         post: post,
         recent_comments: recent_comments.map do |comment|
