3879825f9b86f22005a0cce043b1354212418cc5
nasubibocchi <matsuhito125@gmail.com>
2025-08-02 16:01:23 +0900

    問題1

--- app/services/dashboard_service.rb
+++ app/services/dashboard_service.rb
@@ -1,13 +1,18 @@
 class DashboardService
   def self.blog_dashboard
-    posts = Post.where(status: 'published').limit(10)
+    # posts = Post.where(status: 'published').limit(10)
+    posts = Post.eager_load(:user, :category)
+                .preload(:comments, :tags)
+                .published
+                .limit(10)
 
     dashboard_data = posts.map do |post|
       {
         title: post.title,
         author: post.user.name,
         category: post.category.name,
-        comment_count: post.comments.where(status: 'approved').count,
+        # comment_count: post.comments.where(status: 'approved').count,
+        comment_count: post.comments.approved.count,
         tag_names: post.tags.pluck(:name),
         latest_comment:post.comments.order(created_at: :desc).first&.content
       }
