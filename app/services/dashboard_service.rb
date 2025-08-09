1b67ea486994819489fdcb4ce69b567c383bca29
nasubibocchi <matsuhito125@gmail.com>
2025-08-09 13:38:15 +0900

    問題1: roopのなかで追加クエリが発生するメソッド（count, order, where, pluck）が残っていたので改善

--- app/services/dashboard_service.rb
+++ app/services/dashboard_service.rb
@@ -7,14 +7,19 @@ class DashboardService
                 .limit(10)
 
     dashboard_data = posts.map do |post|
+      approved_comments = post.comments.select{ |c| c.status == "approved" }
+      latest_comment = post.comments.max_by(&:created_at)
+      
       {
         title: post.title,
         author: post.user.name,
         category: post.category.name,
         # comment_count: post.comments.where(status: 'approved').count,
-        comment_count: post.comments.approved.count,
-        tag_names: post.tags.pluck(:name),
-        latest_comment:post.comments.order(created_at: :desc).first&.content
+        comment_count: approved_comments&.size,
+        # tag_names: post.tags.pluck(:name),
+        tag_names: post.tags.map(&:name),
+        # latest_comment:post.comments.order(created_at: :desc).first&.content
+        latest_comment: latest_comment&.content
       }
     end
 
